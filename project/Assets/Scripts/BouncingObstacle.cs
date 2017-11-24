ac8a715ce2c9d9c057b50c9b8d504ad0dfcb65ad
Tiphaine <tiphainejh@hotmail.fr>
2017-11-24 16:43:29 +0100

    creation map

---
+++ project/Assets/Scripts/BouncingObstacle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingObstacle : MonoBehaviour {
+
+	public float speed;
+	void OnCollisionEnter(Collision other)
+	{
+		other.rigidbody.AddForce((-other.contacts [0].normal)*speed,ForceMode.Impulse);
+	}
+}
---
+++ project/Assets/Scripts/ColliderCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderCube : MonoBehaviour {
+
+	public GameObject player;
+
+	void Start()
+	{
+		player = GameObject.Find ("Character");
+	}
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Character")
+			Physics.IgnoreCollision (player.GetComponent<CapsuleCollider>(), GetComponent<BoxCollider>());
+	}
+}
