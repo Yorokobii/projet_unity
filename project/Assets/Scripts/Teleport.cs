87ad94e8fcebb69fae6f12a12ff924b72f9d20d9
Tiphaine <tiphainejh@hotmail.fr>
2017-11-26 12:15:33 +0100

    j'ai continué la map

---
+++ project/Assets/Scripts/Teleport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public Transform destination;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Character")
+        {
+            other.transform.position = destination.position;
+        }
+    }
+}
