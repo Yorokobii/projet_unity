c49cbbd3e6f971ef8970737476855b4d10746df5
Tiphaine <tiphainejh@hotmail.fr>
2017-11-30 15:15:47 +0100

    jai fais une tres belle fonction de terrain

---
+++ project/Assets/Scripts/TerrainBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeTerrain
+{
+	rapide,
+	lent, 
+	glissant,
+	grimpant
+}
+public class TerrainBehavior : MonoBehaviour {
+
+	public TypeTerrain typeTerrain;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Character")
+		{
+			switch (typeTerrain)
+			{
+				case TypeTerrain.rapide:
+					{
+						Debug.Log ("rapide");
+						break;
+					}
+				case TypeTerrain.lent:
+					{					
+						Debug.Log ("lent");
+						break;
+					}
+				case TypeTerrain.glissant:
+					{					
+						Debug.Log ("glissant");
+						break;
+					}
+				case TypeTerrain.grimpant:
+					{					
+						Debug.Log ("grimpant");
+						break;
+					}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Character")
+		{
+			switch (typeTerrain)
+			{
+			case TypeTerrain.rapide:
+				{
+					Debug.Log ("rapide");
+					break;
+				}
+			case TypeTerrain.lent:
+				{					
+					Debug.Log ("lent");
+					break;
+				}
+			case TypeTerrain.glissant:
+				{					
+					Debug.Log ("glissant");
+					break;
+				}
+			case TypeTerrain.grimpant:
+				{					
+					Debug.Log ("grimpant");
+					break;
+				}
+			}
+		}
+	}
+}
