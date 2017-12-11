9e881d1f5da1ba4024d93c37c3bf5bbc409e9d5d
TiphaineJh <Tiphainejh@hotmail.fr>
2017-12-11 14:59:44 +0100

    je sais plus ce que javais modifié

--- project/Assets/Scripts/GameLogic.cs
+++ project/Assets/Scripts/GameLogic.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -32,3 +33,34 @@ public class GameLogic : MonoBehaviour {
 		}
 	}
 }
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+
+	public GameObject enemy;
+	public GameObject Cube;
+	private bool benemy=true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log(Time.time%5);
+		if (((int)Time.time) % 5 > 0) {
+			if (benemy) {
+				//Instantiate (enemy, new Vector3 (0.0f, 1.5f, 0.0f), Quaternion.identity);
+				//Instantiate (Cube, new Vector3 (0.0f, 1.5f, 0.0f), Quaternion.identity);
+				benemy = false;
+			}
+		} else {
+			benemy = true;
+		}
+	}
+}
+>>>>>>> Stashed changes
