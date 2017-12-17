ff9e60e4724618a0403b107c3d25df2d86b6de1c
Yorokobii <lennonaddicts@hotmail.fr>
2017-12-17 23:52:23 +0100

    fixed the merge conflicts that were blocking everything

--- project/Assets/Scripts/GameLogic.cs
+++ project/Assets/Scripts/GameLogic.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -33,34 +32,3 @@ public class GameLogic : MonoBehaviour {
 		}
 	}
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameLogic : MonoBehaviour {
-
-	public GameObject enemy;
-	public GameObject Cube;
-	private bool benemy=true;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//Debug.Log(Time.time%5);
-		if (((int)Time.time) % 5 > 0) {
-			if (benemy) {
-				//Instantiate (enemy, new Vector3 (0.0f, 1.5f, 0.0f), Quaternion.identity);
-				//Instantiate (Cube, new Vector3 (0.0f, 1.5f, 0.0f), Quaternion.identity);
-				benemy = false;
-			}
-		} else {
-			benemy = true;
-		}
-	}
-}
->>>>>>> Stashed changes
