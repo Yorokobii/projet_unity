64ccfeae677f1776198648d6a7b5c6d91fb55236
Yorokobii <lennonaddicts@hotmail.fr>
2017-12-24 13:49:52 +0100

    iu

---
+++ project/Assets/Scripts/billboard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class billboard : MonoBehaviour
+{
+
+	public GameObject character;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+//		float angle = Vector3.Angle(transform.position, character.transform.position);
+//		Vector3 rot = Vector3.RotateTowards(transform.position, character.transform.position, 0, 0);
+//		transform.Rotate(rot);
+//		Quaternion.RotateTowards(transform.rotation, character.transform.rotation, 0);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, character.transform.rotation, 360);
+		
+	}
+}
---
+++ project/Assets/Scripts/particle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particle : MonoBehaviour
+{
+	private float timer;
+	public float aliveTime;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		if(aliveTime >= 0)
+			timer = aliveTime + Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (aliveTime > 0 || !gameObject.GetComponent<ParticleSystem>().isPlaying)
+			if(aliveTime <= 0 || timer <= Time.time)
+				Destroy(gameObject);
+	}
+}
