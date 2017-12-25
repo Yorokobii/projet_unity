376d8c4180c92ca7b952d4447daf45650de9487b
Yorokobii <lennonaddicts@hotmail.fr>
2017-12-25 19:38:23 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaah

--- project/Assets/Scripts/Enemy.cs
+++ project/Assets/Scripts/Enemy.cs
@@ -5,36 +5,90 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class Enemy : MonoBehaviour {
 	private GameObject m_player;
+	public GameObject laser_prefab;
+	public bool shoot;
 	public int HP;
 	public int speed;
 	public int dmg;
 	public int damage_body;
 	public int damage_head;
 	public int range;
+	public int range_shoot;
+	private float timer = -1;
+	private float timer_audio = -1;
+	private bool death_sound = true;
+
+	public AudioClip walk;
+	public AudioClip damage_audio;
+	public AudioClip death;
 
 	// Use this for initialization
 	void Start ()
 	{
 		m_player = GameObject.Find("Character");
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (HP <= 0)
-			die();
+		if (HP <= 0) {
+			if (death_sound) {
+				gameObject.GetComponent<AudioSource> ().PlayOneShot (death);
+				death_sound = false;
+			}
+			die ();
+		}
+
+		if ((timer_audio!=-1) && (timer_audio <= Time.time)) {
+			gameObject.GetComponent<AudioSource> ().clip = walk;
+			gameObject.GetComponent<AudioSource> ().Play();
+			timer_audio = -1;
+		}
 		Vector3 dir = m_player.transform.position - GetComponent<Rigidbody>().transform.position;
 		dir.y = 0;
 		if (dir.magnitude < range)
 		{
+			float mag = dir.magnitude;
+			Vector3 tmp = dir;
 			dir.Normalize();
 			transform.Translate(dir * Time.deltaTime * (speed / 100));
+			if (shoot)
+			{
+				if (dir.magnitude < range_shoot) {
+					if (timer == -1) 
+						timer = Time.time + 5;
+					laser_prefab.transform.localScale = new Vector3 (laser_prefab.transform.localScale.x, mag/2, laser_prefab.transform.localScale.z);
+//					laser_prefab.transform.position = new Vector3 (this.transform.position.x+tmp.x/2, this.transform.position.y+tmp.y/2, this.transform.position.z+tmp.z/2);
+//					laser_prefab.transform.rotation = Quaternion.Euler(new Vector3 (90, -(tmp.x+tmp.z)*2, 0));
+
+					Vector3 targetDir = m_player.transform.position - laser_prefab.transform.position;
+					Vector3 newDir = Vector3.RotateTowards(laser_prefab.transform.up, targetDir, 360, 0.0F);
+					Debug.DrawRay(laser_prefab.transform.position, newDir, Color.red);
+					Quaternion rot = Quaternion.LookRotation(newDir);
+					laser_prefab.transform.rotation = rot;
+
+
+					if (timer <= Time.time) {
+						//m_player.GetComponent<CustomCharacterController> ().damage (dmg);
+						timer = -1;
+//						laser_prefab.GetComponent<MeshRenderer> ().enabled = false;
+					}
+				} else {
+					timer = -1;
+//					laser_prefab.GetComponent<MeshRenderer> ().enabled = false;
+				}
+			}
 		}
-}
+	}
 
 	public void damage(int value){
 		HP -= value;
-		Debug.Log (value);
+		gameObject.GetComponent<AudioSource> ().clip=null;
+		if (HP > 0) {
+			timer_audio = Time.time + damage_audio.length;
+			gameObject.GetComponent<AudioSource> ().PlayOneShot (damage_audio);
+		}
 	}
 
 	public void OnCollisionEnter(Collision other)
@@ -55,6 +109,13 @@ public class Enemy : MonoBehaviour {
 	}
 
 	public void die(){
-		Destroy (gameObject);
+		if (timer_audio==-1)
+			timer_audio = Time.time + death.length;
+		if (timer_audio > Time.time)
+			gameObject.GetComponent<Rigidbody> ().detectCollisions = false;
+		else {
+			timer_audio = -1;
+			Destroy (gameObject);
+		}
 	}
 }
