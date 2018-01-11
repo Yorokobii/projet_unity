d5925d7d64a4c56b715ec7abb21a6d5b70e2883f
Tiphaine <tiphainejh@hotmail.fr>
2018-01-11 12:00:07 +0100

    Ajout des scenes

---
+++ project/Assets/Scripts/KeepOnPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepOnPlatform : MonoBehaviour
+{
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Character")
+		other.transform.parent = gameObject.transform;
+	}
+
+	void OnTriggerExit (Collider other ) {
+		if (other.tag == "Character")
+			other.transform.parent = null;
+	}
+}
---
+++ project/Assets/Scripts/movingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour {
+	public Vector3 pointB;
+	public float speed;
+	private Vector3 pointA;
+
+	IEnumerator Start () {
+		pointA = transform.localPosition;
+
+		while (true)
+		{
+			yield return StartCoroutine(MoveObject(transform, pointA, pointB, 3.0f));
+			yield return StartCoroutine(MoveObject(transform, pointB, pointA, 3.0f));
+		}
+	}
+
+	IEnumerator MoveObject (Transform thisTransform, Vector3 startPos, Vector3 endPos , float time) {
+		float i = 0.0f;
+		float rate = speed/time;
+		while (i < 1.0f) {
+			i += Time.deltaTime * rate;
+			thisTransform.localPosition = Vector3.Lerp(startPos, endPos, i);
+			yield return null; 
+		}
+	}
+		
+}
